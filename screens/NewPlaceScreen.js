d096f615ae333cf53742b1678cc81c922e22428a
blazehub <blaze.avinash@gmail.com>
2020-03-31 10:09:39 +0530

    add place screen navigation

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, StyleSheet, TextInput, Button } from 'react-native';
+import Colors from '../constants/Colors';
 
 const NewPlaceScreen = () => {
-    return (
-        <View>
+    const [titleValue, setTitleValue] = useState('');
 
-        </View>
+    const titleChangeHandler = text => setTitleValue(text);
+
+    const savePlaceHandler = () => { }
+    
+    return (
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput}
+                    onChangeText={titleChangeHandler}
+                    value={titleValue} />
+                <Button title="Save Place"
+                    color={Colors.primary}
+                    onPress={savePlaceHandler} />
+            </View>
+        </ScrollView>
     )
 }
 
-const styles = StyleSheet.create({});
+NewPlaceScreen.navigationOptions = navData => {
+    return {
+        title: 'Add Place'
+    }
+}
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 40
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
 
 export default NewPlaceScreen;
--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
 
 const PlacesListScreen = props => {
     return (
@@ -9,8 +13,16 @@ const PlacesListScreen = props => {
     )
 }
 
-PlacesListScreen.navigationOptions = {
-    title: 'All Places'
+PlacesListScreen.navigationOptions = navData => {
+    return {
+        title: 'All Places',
+        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Add Place' iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                onPress={() => {
+                    navData.navigation.navigate('NewPlace');
+                }} />
+        </HeaderButtons>
+    }
 }
 
 const styles = StyleSheet.create({});
